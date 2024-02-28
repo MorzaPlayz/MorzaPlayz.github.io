042fcf3dc291a96381e443e7690e01cabd5657aa
MorzaPlayz <76044059+MorzaPlayz@users.noreply.github.com>
2024-02-28 22:15:04 +0300

    js test

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-  let x = x1(x,1);
+function testone(){
 
-  function x1(a,b){
-    a+b;
-    return document.getElementById("demo").innerHTML = x;
-  }
+   // const input = document.getElementById("myInput");
+   // alert("Hi " + input.value);
+  
+  
+
+   // document.getElementById("demo").innerHTML = input.Value;
+
+ 
+
+ //var paragraph = document.getElementById('demo');
+  //  paragraph.innerHTML = input.value;
+
+   // alert( paragraph.innerHTML.value);
+   window.location.href="Logged_in.html";
+ 
+
+    
+
+    
+}
+
+
+window.onload = function() {
+
+
+    alert( "test");
+
+
+   const input = document.getElementById("myInput");
+   // alert("Hi " + input.value);
+  
+  
+
+   // document.getElementById("demo").innerHTML = input.Value;
+
+ 
+
+   var paragraph = document.getElementById('#demo');
+    paragraph.innerHTML = input.value;
+
+    
+
+    
+ }
